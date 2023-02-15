80bf0a0773b68acb607f2ab9fe32a5c2cbf92a36
KanHar <knaan.harpaz@gmail.com>
2023-02-15 20:38:33 +0200

    Base case gaussian elimination

--- packages/math-backend/src/Algebra/SquareMatrix.purs
+++ packages/math-backend/src/Algebra/SquareMatrix.purs
@@ -3,10 +3,11 @@ module Algebra.SquareMatrix where
 import Prelude
 import Algebra.Matrix (Matrix(..), madd, matmul, mindex, minitialize, mtoMathJax)
 import Algebra.MyDivisionRing (class MyDivisionRing)
+import Algebra.MyField (class MyField)
 import Algebra.Vector (Vector(..))
-import Data.Array (index, range, zipWith)
+import Data.Array (findIndex, index, range, zipWith)
 import Data.Foldable (sum)
-import Data.Maybe (Maybe(..))
+import Data.Maybe (Maybe(..), fromMaybe)
 import Data.Tuple (Tuple(..))
 import Data.Typelevel.Num (class Nat, class Pos, class Succ, D1, toInt)
 import Data.Typelevel.Undefined (undefined)
@@ -50,8 +51,8 @@ minor (SquareMatrix m) i j =
               matrix_entry
         )
 
-class Determinantable d a where
-  det :: d -> a
+class Determinantable m a where
+  det :: m -> a
 
 instance orderOneDeterminantable :: (MyDivisionRing a) => Determinantable (SquareMatrix D1 a) a where
   det (SquareMatrix m) = case mindex m 0 0 of
@@ -69,3 +70,42 @@ else instance induciveDeterminantable :: (MyDivisionRing a, Pos s, Pos sn, Succ 
       minors_and_idx = map (\i -> Tuple (det (minor (SquareMatrix m) 0 i)) (if i `mod` 2 == 0 then one else (negate one))) (range 0 (toInt (undefined :: s) - 1))
     in
       sum $ zipWith (\(Tuple m_det sign) x -> sign * m_det * x) minors_and_idx (toArray first_row)
+
+class GaussianEliminatable m where
+  gaussianElimination :: m -> { eliminated :: m, transformation :: m, rank :: Int }
+
+instance orderOneGaussianElimination :: (MyField a, Eq a) => GaussianEliminatable (SquareMatrix D1 a) where
+  gaussianElimination (SquareMatrix m) =
+    let
+      v = case mindex m 0 0 of
+        Just x -> x
+        Nothing -> error "Matrix index out of bounds"
+    in
+      { eliminated: SquareMatrix m, transformation: SquareMatrix $ minitialize (\_ _ -> one), rank: if v == zero then 0 else 1 }
+else instance inductive :: (MyField a, Eq a, Pos s, Pos sn, Succ s sn, GaussianEliminatable (SquareMatrix s a)) => GaussianEliminatable (SquareMatrix sn a) where
+  gaussianElimination (SquareMatrix m) =
+    let
+      pivot =
+        findIndex
+          ( \i -> case mindex m i 0 of
+              Just x -> x /= zero
+              Nothing -> error "Matrix index out of bounds"
+          )
+          $ range 0 (toInt (undefined :: s) - 1)
+    in
+      case pivot of
+        Just _ -> error "Not implemented yet"
+        Nothing ->
+          let
+            remaining_matrix = minor (SquareMatrix m) 0 0
+
+            remaining_elimination = gaussianElimination remaining_matrix
+
+            eliminated_rest_of_matrix = (\(SquareMatrix m) -> m) remaining_elimination.eliminated
+
+            transformation_rest_of_matrix = (\(SquareMatrix m) -> m) remaining_elimination.transformation
+          in
+            { eliminated: SquareMatrix $ minitialize (\i j -> if i == 0 || j == 0 then fromMaybe zero $ mindex m i j else fromMaybe zero $ mindex eliminated_rest_of_matrix (i - 1) (j - 1))
+            , transformation: SquareMatrix $ minitialize (\i j -> if i == 0 || j == 0 then (if i == 0 && j == 0 then one else zero) else fromMaybe zero $ mindex transformation_rest_of_matrix (i - 1) (j - 1))
+            , rank: remaining_elimination.rank
+            }
